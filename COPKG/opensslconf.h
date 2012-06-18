13da59b033fdba420fbaeaf3fd525db31aedee11
Tim Rogers <tiroger@microsoft.com>
2012-06-18 14:29:28 -0700

    fixed include issue

--- COPKG/opensslconf.h
+++ COPKG/opensslconf.h
@@ -1,23 +1,20 @@
 // Stub include header to auto-include the correct version 
 //  of opensslconf.h for the target architecture.
 
-#ifndef OPENSSLCONF_BRIDGE
-#define OPENSSLCONF_BRIDGE
-#  if defined(_MSC_VER) 
-#    if defined(_M_IA64) || defined(_M_X64) || defined(_WIN64)
+#if defined(_MSC_VER) 
+#  if defined(_M_IA64) || defined(_M_X64) || defined(_WIN64)
 #include <openssl/opensslconf_x64.h>
-#    else 
-#include <openssl/opensslconf_x86.h>
-#    endif 
-#  elif defined(__GNUG__) 
-#    if defined(__x86_64__) || defined(__ia64__) 
-#include <openssl/opensslconf_x64.h>
-#    else 
-#include <openssl/opensslconf_x86.h>
-#    endif 
-#  elif defined(_x64) || defined(x64)
-#include <openssl/opensslconf_x64.h>
-#  else
+#  else 
 #include <openssl/opensslconf_x86.h>
 #  endif 
+#elif defined(__GNUG__) 
+#  if defined(__x86_64__) || defined(__ia64__) 
+#include <openssl/opensslconf_x64.h>
+#  else 
+#include <openssl/opensslconf_x86.h>
+#  endif 
+#elif defined(_x64) || defined(x64)
+#include <openssl/opensslconf_x64.h>
+#else
+#include <openssl/opensslconf_x86.h>
 #endif 
