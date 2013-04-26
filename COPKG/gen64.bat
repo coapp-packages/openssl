ff191216d8187ac98ce92c46d859d4c7f598debe
Tim Rogers <tiroger@microsoft.com>
2013-04-26 09:29:08 -0700

    changes to meet current standards

--- COPKG/gen64.bat
+++ COPKG/gen64.bat
@@ -23,11 +23,12 @@ mkdir output\%1\x64
 mkdir output\%1\x64\Release
 mkdir output\%1\x64\Release\include
 
-perl Configure no-rc5 no-idea enable-mdc2 enable-zlib VC-WIN64A -I"%2/lib/native/include" -L"%2/lib/native/lib/x64/%1/%3"
+REM perl Configure no-rc5 no-idea enable-mdc2 enable-zlib VC-WIN64A -I"%2/lib/native/include" -L"%2/lib/native/lib/x64/%1/%3"
+perl Configure no-rc5 no-idea enable-mdc2 zlib-dynamic VC-WIN64A -I"%2/lib/native/include" -L"%2/lib/native/lib/x64/%1/%3"
 call ms\do_win64a
 powershell -command "cat ms\ntdll.mak | ForEach-Object{ $_ -replace \"out32dll\", \"output/%1/x64/Release\" } | ForEach-Object{ $_ -replace \"tmp32dll\", \"intermediate/%1/x64/Release\" } | ForEach-Object{ $_ -replace \"inc32\", \"output/%1/x64/Release/include\" } | sc ms\ntdll64.mak"
 
-
+pause
 nmake -f ms\ntdll64.mak	|| goto failed
 
 
