45b95a5fd3620cc5a4ff6885db19fbadff7b425d
Tim Rogers <tiroger@microsoft.com>
2013-05-02 10:00:33 -0700

    corrections to build files

--- COPKG/all.bat
+++ COPKG/all.bat
@@ -8,36 +8,36 @@ if "%1"=="noclean" (
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
-call :build x64 Release v110
-call :build x64 Debug v110
+call :build x64 Release v110 || goto :eof
+call :build x64 Debug v110 || goto :eof
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-call :build Win32 Release v110
-call :build Win32 Debug v110
+call :build Win32 Release v110 || goto :eof
+call :build Win32 Debug v110 || goto :eof
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
-call :build x64 Release v100
-call :build x64 Debug v100
+call :build x64 Release v100 || goto :eof
+call :build x64 Debug v100 || goto :eof
 endlocal
 
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-call :build Win32 Release v100
-call :build Win32 Debug v100
+call :build Win32 Release v100 || goto :eof
+call :build Win32 Debug v100 || goto :eof
 endlocal
 
-if "__NOCLEAN__"=="true" goto :eof
+if "%__NOCLEAN__%"=="true" goto :eof
 
 goto :clean
 
 :build
-msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary .\openssl.sln
-msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary .\openssl.sln
-msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=ltcg .\openssl.sln
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary .\openssl.sln || goto :eof
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary .\openssl.sln || goto :eof
+REM msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=ltcg .\openssl.sln || goto :eof
 goto :eof
 
 :clean
